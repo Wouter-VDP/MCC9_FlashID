8f6d27212ac8696dbc24517138f3255c66c6e997
Wouter-VDP <woutervdp@gmail.com>
2018-11-26 12:50:48 +0000

    added single event viewer

--- helpfunction.py
+++ helpfunction.py
@@ -77,3 +77,99 @@ def hist_bin_uncertainty(data, weights, bin_edges):
         [np.sqrt(np.sum(np.square(w))) for w in binned_weights])
     return bin_uncertainties
 
+
+# This cell is a single event viewer!
+def SingleEventViewer(sample,run,subrun,event):
+    str_pause = '------------------------------'
+    event_keys = [b'nFlashes', b'hasBeamFlash', b'nSlices', b'nSlicesAfterPrecuts', b'foundATargetSlice', 
+                  b'nuCCNC', b'nuEnergy', b'leptonEnergy', b'nuInteractionTime', b'nuPdgCode', b'nuVertexX', b'nuVertexY', b'nuVertexZ']
+    flash_keys = [b'time', b'centerY', b'centerZ', b'widthY', b'widthZ', b'totalPE', b'inBeamWindow', b'isBeamFlash']
+    slice_keys = [b'hasDeposition', b'totalCharge', b'centerX', b'centerY', b'centerZ', b'minX', b'nHits',
+                  b'deltaY', b'deltaZ', b'deltaYSigma', b'deltaZSigma', b'chargeToLightRatio', 
+                  b'passesPreCuts', b'flashMatchScore', b'flashMatchX', b'totalPEHypothesis', 
+                  b'isTaggedAsTarget', b'isConsideredByFlashId', b'topologicalScore', b'hasBestTopologicalScore', 
+                  b'purity', b'completeness']
+    # Event Info:
+    event_dict = file_dict[sample]['events']
+    events_index = np.where( (event_dict[b'run']==run) & (event_dict[b'subRun']==subrun) & (event_dict[b'event']==event))
+    if len(events_index[0])!=1:
+        print('The combination of event, subrun and run was not found in sample',sample,'.')
+        return
+    print('Run',run,', Subrun', subrun,', Event',event,' found!','\n',str_pause)
+    print('\n--- EVENT INFO ---')
+    events_index = events_index[0][0]
+    for key in event_keys:
+        print(key.decode('UTF-8'), ':\t', event_dict[key][events_index])
+        
+    # Flash Info:
+    flash_dict = file_dict[sample]['flashes']
+    flashes_indices = np.where( (flash_dict[b'run']==run) & (flash_dict[b'subRun']==subrun) & (flash_dict[b'event']==event))[0]
+    print('\n--- FLASH INFO ---')
+    for key in flash_keys:
+        print(key.decode('UTF-8'), ':\t', flash_dict[key][flashes_indices])
+    
+    # Slice Info:
+    slice_dict = file_dict[sample]['slices']
+    slices_indices = np.where( (slice_dict[b'run']==run) & (slice_dict[b'subRun']==subrun) & (slice_dict[b'event']==event))[0]
+    print('\n--- SLICE INFO ---')
+    for key in slice_keys:
+        print(key.decode('UTF-8'), ':\t', slice_dict[key][slices_indices])
+    
+    if not np.any(flash_dict[b'isBeamFlash'][flashes_indices]):
+        print('\nUnable to plot: There was no beamflash in the selected event!')
+        return
+    # Make the plot!
+    fig, ax = plt.subplots(figsize=(5.5*gr,5.5))
+    ax.set_xlabel('PMT identification number')
+    ax.set_ylabel('Number of Photo-electrons per PMT')
+    ax.grid(alpha=.3)
+    
+    beam_flash_index = flashes_indices[flash_dict[b'isBeamFlash'][flashes_indices]][0]
+    beam_flash_pe = flash_dict[b'peSpectrum'][beam_flash_index]
+    
+    lab_flash = "\nOptical Flash\n  PE: {0:.0f} \n  ".format(flash_dict[b'totalPE'][beam_flash_index]) \
+                +r"$z$: {0:.0f} cm".format(flash_dict[b'centerZ'][beam_flash_index])+"\n"
+    
+    ax.errorbar(rangePMT,beam_flash_pe, yerr=np.sqrt(beam_flash_pe), fmt="none")
+    ax.fill_between(rangePMT, beam_flash_pe, alpha=.5,label=lab_flash)
+    
+    
+    slice_hypo_index = slices_indices[slice_dict[b'passesPreCuts'][slices_indices]]
+    flash_hypo_pe = slice_dict[b'peHypothesisSpectrum'][slice_hypo_index]
+    for i,(idx, spectrum) in enumerate(zip(slice_hypo_index, flash_hypo_pe)):
+        slice_lab = 'Slice Hypothesis '+str(i)+"\n  "
+        slice_lab+=r"Purity: "+ str(round(slice_dict[b'purity'][idx],1)) + "\n  "
+        slice_lab+=r"Completeness: "+ str(round(slice_dict[b'completeness'][idx],3)) + "\n  "
+        slice_lab+=r"Topo score: "+ str(round(slice_dict[b'topologicalScore'][idx],3)) + "\n  "
+        slice_lab+=r"Flash score: {0:.2e}".format(slice_dict[b'flashMatchScore'][idx])
+        slice_lab+="\n  "+r"$\Delta$z: "+ str(round(slice_dict[b'deltaZ'][idx],1)) + "cm\n"
+        ax.errorbar(rangePMT,spectrum, yerr= np.sqrt(spectrum), label=slice_lab) 
+
+    ax.legend(bbox_to_anchor=(1.02,0.2,.25,.8),loc=2)
+    
+    ax.set_title('Run '+str(run)+', Subrun '+str(subrun)+', Event '+str(event), loc='left')
+    if (b'nuPdgCode' in event_dict):
+        d_pdg = {12: r"$\nu_e$", 14: r"$\nu_\mu$",-12: r"$\bar{\nu_e}$", -14: r"$\bar{\nu_\mu}$"}
+        print(str_pause,"\nProducing plot for MC event!")
+        ax.set_title("MicroBooNE Simulation", loc='right')
+        txt =  d_pdg[event_dict[b'nuPdgCode'][events_index]]
+        txt+= " with {0:.2f} GeV energy\n".format(event_dict[b'nuEnergy'][events_index])
+        txt+= "and vertex: ({0:.0f}, {1:.0f}, {2:.0f}) cm".format(event_dict[b'nuVertexX'][events_index],
+                                     event_dict[b'nuVertexY'][events_index],
+                                     event_dict[b'nuVertexZ'][events_index])
+        x_txt_start = 1
+        if np.argmax(beam_flash_pe)<nrPMT/2:
+            x_txt_start = 17
+        
+        ax.text(x_txt_start, 0.8*ax.get_ylim()[1], txt, fontsize=12)
+        ax.set_ylim(0,ax.get_ylim()[1])
+        ax.set_xlim(-0.5,31.5)
+    else:
+        print(str_pause,"\nProducing plot for Data event!")
+        ax.set_title("MicroBooNE Data", loc='right')
+        
+    fig.tight_layout()
+    if 1:
+        tag = "run"+str(run)+"_subrun"+str(subrun)+"_event"+str(event)
+        fig.savefig("event_viewer_"+tag+".pdf", bbox_inches="tight")
+        print("Image saved: "+"event_viewer_"+tag+".pdf" )
